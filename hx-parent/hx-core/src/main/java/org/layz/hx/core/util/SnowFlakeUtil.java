918b59592fe224805280cd747415767f0588e623
wyhx21 <2247819253@qq.com>
2020-05-24 21:13:59 +0800

    雪花算法更新

--- hx-parent/hx-core/src/main/java/org/layz/hx/core/util/SnowFlakeUtil.java
+++ hx-parent/hx-core/src/main/java/org/layz/hx/core/util/SnowFlakeUtil.java
@@ -1,41 +1,99 @@
 package org.layz.hx.core.util;
 
-import org.layz.hx.core.pojo.common.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class SnowFlakeUtil {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SnowFlakeUtil.class);
-    private static SnowFlake snowFlake;
+    private static SnowFlakeUtil snowFlake;
+    private static final Logger LOOGER = LoggerFactory.getLogger(SnowFlakeUtil.class);
 
-    public static SnowFlake getSnowFlake(){
-        if(null != snowFlake) {
-            return snowFlake;
+    private final long workerId;
+    private final long epoch = 1590325575378L; // 时间起始标记点，作为基准，一般取系统的最近时间
+    private final long workerIdBits = 10L; // 机器标识位数
+    private final long maxWorkerId = -1L ^ -1L << this.workerIdBits;// 机器ID最大值:
+    // 1023
+    private long sequence = 0L; // 0，并发控制
+    private final long sequenceBits = 12L; // 毫秒内自增位
+
+    private final long workerIdShift = this.sequenceBits; // 12
+    private final long timestampLeftShift = this.sequenceBits + this.workerIdBits;// 22
+    private final long sequenceMask = -1L ^ -1L << this.sequenceBits; // 4095,111111111111,12位
+    private long lastTimestamp = -1L;
+
+    private SnowFlakeUtil() {
+        long workerId = 156;
+        try {
+
+            InetAddress ia = InetAddress.getLocalHost();
+            NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+
+            byte[] mac = ni.getHardwareAddress();
+            String s = Integer.toHexString(mac[mac.length - 1] & 0xFF);
+            int macWorker = Integer.parseInt(s, 16);
+            if (macWorker < 1030) {
+                workerId = macWorker;
+            }
+        } catch (SocketException e) {
+            LOOGER.error("soket exception : ", e);
+        } catch (UnknownHostException e) {
+            LOOGER.error("UnknownHostException : ", e);
         }
-        init();
+
+        if (workerId > this.maxWorkerId || workerId < 0) {
+            LOOGER.error("!!!!!!!!!!!workerId : {} --maxWorkerId:{} !!!!!!!!! ", workerId, this.maxWorkerId);
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", this.maxWorkerId));
+        }
+        this.workerId = workerId;
+    }
+
+    public static SnowFlakeUtil getSnowFlake(){
+        if (snowFlake == null) {
+            snowFlake = new SnowFlakeUtil();
+        }
         return snowFlake;
     }
 
-    private static synchronized void init() {
-        if(null != snowFlake) {
-            return;
+    public synchronized long nextId() {
+        long timestamp = SnowFlakeUtil.timeGen();
+        if (timestamp < this.lastTimestamp) {
+            throw new RuntimeException(String.format("!!!!!!!!!!Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
         }
-        int machineId = 1;
-        try {
-            InetAddress ia = InetAddress.getLocalHost();
-            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(ia);
-            byte[] mac = networkInterface.getHardwareAddress();
-            String str = Integer.toHexString(mac[mac.length -1] & 255);
-            int parseInt = Integer.parseInt(str, 16);
-            if(parseInt <1030) {
-                machineId = parseInt;
+
+        // 如果上一个timestamp与新产生的相等，则sequence加一(0-4095循环);
+        if (this.lastTimestamp == timestamp) {
+            // 对新的timestamp，sequence从0开始
+            this.sequence = this.sequence + 1 & this.sequenceMask;
+            if (this.sequence == 0) {
+                timestamp = this.tilNextMillis(this.lastTimestamp);// 重新生成timestamp
             }
-        } catch (Throwable e) {
-            LOGGER.error("", e);
+        } else {
+            this.sequence = 0;
         }
-        snowFlake = new SnowFlake(machineId);
+
+        this.lastTimestamp = timestamp;
+        return timestamp - this.epoch << this.timestampLeftShift | this.workerId << this.workerIdShift | this.sequence;
+    }
+
+    /**
+     * 等待下一个毫秒的到来, 保证返回的毫秒数在参数lastTimestamp之后
+     */
+    private long tilNextMillis(long lastTimestamp) {
+        long timestamp = SnowFlakeUtil.timeGen();
+        while (timestamp <= lastTimestamp) {
+            timestamp = SnowFlakeUtil.timeGen();
+        }
+        return timestamp;
+    }
+
+    /**
+     * 获得系统当前毫秒数
+     */
+    private static long timeGen() {
+        return System.currentTimeMillis();
     }
 }
